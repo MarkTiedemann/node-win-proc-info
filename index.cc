ea7a4bc42166069944c2ce476ab46e7ab6f357e7
Mark Tiedemann <www.marktiedemann@gmail.com>
2018-07-07 05:22:37 +0200

    Misc cleanup

--- index.cc
+++ index.cc
@@ -1,85 +1,55 @@
 #include <napi.h>
 #include <windows.h>
-#include <stdio.h>
 #include <psapi.h>
-#include <assert.h>
 
 namespace proc_info
 {
 
-PROCESS_MEMORY_COUNTERS_EX get_mem_info(DWORD pid)
-{
-  HANDLE proc;
-  PROCESS_MEMORY_COUNTERS_EX mem_counters;
-
-  proc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid);
-  GetProcessMemoryInfo(proc, (PROCESS_MEMORY_COUNTERS *)&mem_counters, sizeof(mem_counters));
-  CloseHandle(proc);
-
-  return mem_counters;
-}
-
 void add_prop(napi_env env, napi_value output, char key[], SIZE_T value)
 {
-  napi_status status;
-
-  napi_value prop_key;
-  status = napi_create_string_utf8(env, key, strlen(key), &prop_key);
-  assert(status == napi_ok);
-
   napi_value prop_value;
-  status = napi_create_double(env, (double)value, &prop_value);
-  assert(status == napi_ok);
-
-  status = napi_set_property(env, output, prop_key, prop_value);
-  assert(status == napi_ok);
+  napi_create_double(env, (double)value, &prop_value);
+  napi_set_named_property(env, output, key, prop_value);
 }
 
 napi_value get_proc_info(napi_env env, napi_callback_info info)
 {
-  napi_status status;
-  size_t argc = 2;
+  size_t argc = 1;
   napi_value args[1];
+  napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+
   double pid;
-  napi_value mem_info_obj;
-  PROCESS_MEMORY_COUNTERS_EX mem_info;
+  napi_get_value_double(env, args[0], &pid);
 
-  status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
-  assert(status == napi_ok);
+  auto proc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, (DWORD)pid);
 
-  status = napi_get_value_double(env, args[0], &pid);
-  assert(status == napi_ok);
+  PROCESS_MEMORY_COUNTERS_EX mem_counters;
+  GetProcessMemoryInfo(proc, (PROCESS_MEMORY_COUNTERS *)&mem_counters, sizeof(mem_counters));
 
-  mem_info = get_mem_info((DWORD)pid);
+  CloseHandle(proc);
 
-  status = napi_create_object(env, &mem_info_obj);
-  assert(status == napi_ok);
+  napi_value proc_info;
+  napi_create_object(env, &proc_info);
 
-  add_prop(env, mem_info_obj, "pageFaultCount", mem_info.PageFaultCount);
-  add_prop(env, mem_info_obj, "pagefileUsage", mem_info.PagefileUsage);
-  add_prop(env, mem_info_obj, "peakPagefileUsage", mem_info.PeakPagefileUsage);
-  add_prop(env, mem_info_obj, "peakWorkingSetSize", mem_info.PeakWorkingSetSize);
-  add_prop(env, mem_info_obj, "privateUsage", mem_info.PrivateUsage);
-  add_prop(env, mem_info_obj, "quotaNonPagedPoolUsage", mem_info.QuotaNonPagedPoolUsage);
-  add_prop(env, mem_info_obj, "quotaPagedPoolUsage", mem_info.QuotaPagedPoolUsage);
-  add_prop(env, mem_info_obj, "quotaPeakNonPagedPoolUsage", mem_info.QuotaPeakNonPagedPoolUsage);
-  add_prop(env, mem_info_obj, "quotaPeakPagedPoolUsage", mem_info.QuotaPeakPagedPoolUsage);
-  add_prop(env, mem_info_obj, "workingSetSize", mem_info.WorkingSetSize);
+  add_prop(env, proc_info, "pageFaultCount", mem_counters.PageFaultCount);
+  add_prop(env, proc_info, "pagefileUsage", mem_counters.PagefileUsage);
+  add_prop(env, proc_info, "peakPagefileUsage", mem_counters.PeakPagefileUsage);
+  add_prop(env, proc_info, "peakWorkingSetSize", mem_counters.PeakWorkingSetSize);
+  add_prop(env, proc_info, "privateUsage", mem_counters.PrivateUsage);
+  add_prop(env, proc_info, "quotaNonPagedPoolUsage", mem_counters.QuotaNonPagedPoolUsage);
+  add_prop(env, proc_info, "quotaPagedPoolUsage", mem_counters.QuotaPagedPoolUsage);
+  add_prop(env, proc_info, "quotaPeakNonPagedPoolUsage", mem_counters.QuotaPeakNonPagedPoolUsage);
+  add_prop(env, proc_info, "quotaPeakPagedPoolUsage", mem_counters.QuotaPeakPagedPoolUsage);
+  add_prop(env, proc_info, "workingSetSize", mem_counters.WorkingSetSize);
 
-  return mem_info_obj;
+  return proc_info;
 }
 
 napi_value init(napi_env env, napi_value exports)
 {
-  napi_status status;
   napi_value fn;
-
-  status = napi_create_function(env, nullptr, 0, get_proc_info, nullptr, &fn);
-  assert(status == napi_ok);
-
-  status = napi_set_named_property(env, exports, "proc_info", fn);
-  assert(status == napi_ok);
-
+  napi_create_function(env, nullptr, 0, get_proc_info, nullptr, &fn);
+  napi_set_named_property(env, exports, "get_proc_info", fn);
   return exports;
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const bindings = require('bindings')
+
 const addon = bindings('proc_info.node')
-module.exports = addon.proc_info
+
+module.exports = function procInfo(pid) {
+  if (Number.isSafeInteger(pid)) {
+    return addon.get_proc_info(pid)
+  } else {
+    throw new RangeError('invalid argument: pid must be a safe integer')
+  }
+}
